b86b1ac539f6f9393b241e1b6eeced16c6d8a703
cjanie <janiechun@hotmail.fr>
2021-04-25 15:20:42 +0200

    init

---
+++ src/main/java/com/aliphon_word_webservice/aliphon_word_webservice/AliphonWordWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.aliphon_word_webservice.aliphon_word_webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliphonWordWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AliphonWordWebserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aliphon_word_webservice/aliphon_word_webservice/AliphonWordWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aliphon_word_webservice.aliphon_word_webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AliphonWordWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
